7484bb16aae0b3a318e066ab0b4200340873c605
D0683497 <d0683497@mail.fcu.edu.tw>
2019-08-21 00:23:51 +0800

    complete join page and form

--- join/forms.py
+++ join/forms.py
@@ -10,10 +10,9 @@ LEVEL_CHOICES = (
 )
 
 class JoinForm(forms.Form):
-    name = forms.CharField(label='姓名', required=True, max_length=50)
-    nid = forms.CharField(label='學號', required=True, max_length=15)
-    dept = forms.CharField(label='系級', required=True, max_length=20, help_text="例如:資訊一甲")
-    level = forms.ChoiceField(label='年級', choices=LEVEL_CHOICES, required=True)
-    phone = forms.CharField(label='電話', required=True, max_length=15, help_text="請輸入可以聯絡到您的手機")
-    email = forms.EmailField(label='E-mail', required=True, help_text="請輸入您常用的 E-mail")
-    privacy = forms.BooleanField(label='本人同意將個人資料提供逢甲大學黑客社使用，本社依個人資料保護法、相關法規及學校相關法規進行處理以及利用。', required=True)
+    name = forms.CharField(label='姓名', required=True, max_length=50, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '請輸入姓名'}))
+    nid = forms.CharField(label='學號', required=True, max_length=15, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '請輸入學號'}))
+    dept = forms.CharField(label='系級', required=True, max_length=20, help_text="例如:資訊一甲", widget=forms.TextInput(attrs={'class': 'form-control', 'aria-describedby': 'deptHelp', 'placeholder': '請輸入系級'}))
+    level = forms.ChoiceField(label='年級', choices=LEVEL_CHOICES, required=True, widget=forms.Select(attrs={'class': 'form-control', 'placeholder': ''}))
+    phone = forms.CharField(label='電話', required=True, max_length=15, help_text="請輸入可以聯絡到您的手機", widget=forms.TextInput(attrs={'class': 'form-control', 'aria-describedby': 'phoneHelp', 'placeholder': ''}))
+    email = forms.EmailField(label='E-mail', required=True, help_text="請輸入您常用的 E-mail", widget=forms.EmailInput(attrs={'class': 'form-control', 'aria-describedby': 'emailHelp', 'placeholder': ''}))
