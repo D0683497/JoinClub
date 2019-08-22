a5176b650006cb6af0c75a97a8ef0167e9d1a515
D0683497 <d0683497@mail.fcu.edu.tw>
2019-08-22 22:31:01 +0800

    complete JoinForm

--- join/forms.py
+++ join/forms.py
@@ -1,18 +1,50 @@
 from django import forms
+import re
 
 LEVEL_CHOICES = (
-    ('1', '大一'),
-    ('2', '大二'),
-    ('3', '大三'),
-    ('4', '大四'),
-    ('5', '碩一'),
-    ('6', '碩二')
+    ('B1', '大一'),
+    ('B2', '大二'),
+    ('B3', '大三'),
+    ('B4', '大四'),
+    ('M1', '碩一'),
+    ('M2', '碩二')
 )
 
 class JoinForm(forms.Form):
-    name = forms.CharField(label='姓名', required=True, max_length=50, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '請輸入姓名'}))
-    nid = forms.CharField(label='學號', required=True, max_length=15, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '請輸入學號'}))
-    dept = forms.CharField(label='系級', required=True, max_length=20, help_text="例如:資訊一甲", widget=forms.TextInput(attrs={'class': 'form-control', 'aria-describedby': 'deptHelp', 'placeholder': '請輸入系級'}))
-    level = forms.ChoiceField(label='年級', choices=LEVEL_CHOICES, required=True, widget=forms.Select(attrs={'class': 'form-control', 'placeholder': ''}))
-    phone = forms.CharField(label='電話', required=True, max_length=15, help_text="請輸入可以聯絡到您的手機", widget=forms.TextInput(attrs={'class': 'form-control', 'aria-describedby': 'phoneHelp', 'placeholder': ''}))
-    email = forms.EmailField(label='E-mail', required=True, help_text="請輸入您常用的 E-mail", widget=forms.EmailInput(attrs={'class': 'form-control', 'aria-describedby': 'emailHelp', 'placeholder': ''}))
+    name  = forms.CharField(label='姓名', 
+                            required=True, 
+                            max_length=50, 
+                            widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '請輸入姓名'}), 
+                            error_messages={'required': '必須填寫姓名', 'max_length': '你的姓名太長囉'})
+    nid   = forms.CharField(label='學號', 
+                            required=True, 
+                            max_length=15, 
+                            widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '請輸入學號'}), 
+                            error_messages={'required': '必須填寫學號', 'max_length': '你的學號太長囉'})
+    dept  = forms.CharField(label='系級', 
+                            required=True, 
+                            max_length=20, 
+                            help_text="例如:資訊一甲", 
+                            widget=forms.TextInput(attrs={'class': 'form-control', 'aria-describedby': 'deptHelp', 'placeholder': '請輸入系級'}), 
+                            error_messages={'required': '必須填寫系級', 'max_length': '你的系級太長囉'})
+    level = forms.ChoiceField(label='年級', 
+                            choices=LEVEL_CHOICES, 
+                            required=True, 
+                            widget=forms.Select(attrs={'class': 'form-control', 'placeholder': ''}), 
+                            error_messages={'required': '必須選擇年級', 'invalid_choice': '請勿亂來'})
+    phone = forms.CharField(label='電話', 
+                            required=True, 
+                            max_length=15, 
+                            help_text="請輸入可以聯絡到您的手機", 
+                            widget=forms.TextInput(attrs={'class': 'form-control', 'aria-describedby': 'phoneHelp', 'placeholder': ''}), 
+                            error_messages={'required': '必須填寫電話', 'max_length': '你的電話太長囉'})
+    email = forms.EmailField(label='E-mail', 
+                            required=True, 
+                            help_text="請輸入您常用的 E-mail", 
+                            widget=forms.EmailInput(attrs={'class': 'form-control', 'aria-describedby': 'emailHelp', 'placeholder': ''}), 
+                            error_messages={'required': '必須填寫 E-mail'})
+    
+    #驗證 phone
+    def clean_phone(self):
+        data = self.cleaned_data['phone']
+        return data
