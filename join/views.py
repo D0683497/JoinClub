c5217f460d8f80b8d215457b95cf10bf1fedd540
D0683497 <d0683497@mail.fcu.edu.tw>
2019-08-22 22:31:56 +0800

    form store

--- join/views.py
+++ join/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .forms import JoinForm
+from .models import Member
 
 def index(request):
     return render(request, 'index.html', {})
@@ -8,7 +11,9 @@ def join(request):
     if request.method == 'POST':
         form = JoinForm(request.POST)
         if form.is_valid():
-            pass
+            Member(name=form['name'].value(), nid=form['nid'].value(), dept=form['dept'].value(), level=form['level'].value(), phone=form['phone'].value(), email=form['email'].value()).save()
+            form = JoinForm()
+        return HttpResponseRedirect(reverse('index'))
     else:
         form = JoinForm()
     return render(request, 'join.html', {'form': form})
