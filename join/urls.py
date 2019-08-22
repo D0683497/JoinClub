9dde8c34cb4a6dce7a26ff31d4eccbaab6ab9f0e
D0683497 <d0683497@mail.fcu.edu.tw>
2019-08-23 01:19:13 +0800

    add review,edit url

--- join/urls.py
+++ join/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('join', views.join, name="join"),
+    path('review', views.review, name="review"),
+    path('edit', views.edit, name="edit"),
 ]
