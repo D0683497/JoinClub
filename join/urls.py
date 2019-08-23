754c134603ed740d6309e9a954ddd1507d6c06f2
D0683497 <d0683497@mail.fcu.edu.tw>
2019-08-23 19:11:18 +0800

    add app name and change some link

--- join/urls.py
+++ join/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'join'
+
 urlpatterns = [
     path('', views.index, name="index"),
     path('join', views.join, name="join"),
-    path('review', views.review, name="review"),
-    path('edit', views.edit, name="edit"),
+    path('search', views.search, name="search"),
+    path('review/<int:id>', views.review, name="review"),
+    path('edit/<int:id>', views.edit, name="edit"),
 ]
