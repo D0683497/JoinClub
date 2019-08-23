e0e3ee3cd5a8d8ad56ba168bf466ecc71a5716b3
D0683497 <d0683497@mail.fcu.edu.tw>
2019-08-23 19:13:42 +0800

    chaneg some link and complete edit and take out search to review

--- join/views.py
+++ join/views.py
@@ -13,17 +13,36 @@ def join(request):
         if form.is_valid():
             Member(name=form['name'].value(), nid=form['nid'].value(), dept=form['dept'].value(), level=form['level'].value(), phone=form['phone'].value(), email=form['email'].value()).save()
             form = JoinForm()
-        return HttpResponseRedirect(reverse('index'))
+        return HttpResponseRedirect(reverse('join:index'))
     else:
         form = JoinForm()
     return render(request, 'join.html', {'form': form})
 
-def review(request):
+def search(request):
     if request.method == 'POST':
         member = get_object_or_404(Member, nid=request.POST.get('nid'))
-        print(member)
-        return render(request, 'review.html', {'member': member})
-    return render(request, 'review.html', {})
+        return HttpResponseRedirect(reverse('join:review', args=[member.id]))
+    return render(request, 'search.html', {})
 
-def edit(request):
-    return render(request, 'edit.html', {})
+def review(request, id):
+    member = get_object_or_404(Member, id=id)
+    return render(request, 'review.html', {'member': member})
+
+def edit(request, id):
+    member = get_object_or_404(Member, id=id)
+    if request.method == 'POST':
+        form = JoinForm(request.POST)
+        if form.is_valid():
+            # Member(name=form['name'].value(), nid=form['nid'].value(), dept=form['dept'].value(), level=form['level'].value(), phone=form['phone'].value(), email=form['email'].value()).save()
+            member.name = form['name'].value()
+            member.nid = form['nid'].value()
+            member.dept = form['dept'].value()
+            member.level = form['level'].value()
+            member.phone = form['phone'].value()
+            member.email = form['email'].value()
+            member.save()
+            form = JoinForm()
+        return HttpResponseRedirect(reverse('join:review', args=[member.id]))
+    else:
+        form = JoinForm()
+    return render(request, 'edit.html', {'form': form, 'member': member})
