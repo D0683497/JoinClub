308f00c4681722d379ac1432959bb6abd5136ae1
D0683497 <d0683497@mail.fcu.edu.tw>
2019-08-24 16:07:29 +0800

    add http error page

--- joinclub/settings.py
+++ joinclub/settings.py
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Custom
+from . import views
+CSRF_FAILURE_VIEW = views.csrf_failure
--- joinclub/urls.py
+++ joinclub/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('join.urls')),
 ]
+
+handler400 = views.error_400
+handler403 = views.error_403
+handler404 = views.error_404
+handler500 = views.error_500
