dbd0594b475d840f57caa1b67e0648994172a9de
D0683497 <d0683497@mail.fcu.edu.tw>
2019-08-24 14:16:13 +0800

    move choice in class and change phone to unique

--- join/models.py
+++ join/models.py
@@ -1,26 +1,26 @@
 from django.db import models
 
-LEVEL_CHOICES = [
-    ('B1', '大一'), 
-    ('B2', '大二'), 
-    ('B3', '大三'), 
-    ('B4', '大四'), 
-    ('M1', '碩一'), 
-    ('M2', '碩二'), 
-]
-
-STATUS_CHOICES = [
-    ('UR', '審核中'), 
-    ('M', '已入社'),
-    ('NP', '未付款'),
-]
-
 class Member(models.Model):
+    LEVEL_CHOICES = [
+        ('B1', '大一'), 
+        ('B2', '大二'), 
+        ('B3', '大三'), 
+        ('B4', '大四'), 
+        ('M1', '碩一'), 
+        ('M2', '碩二'), 
+    ]
+
+    STATUS_CHOICES = [
+        ('UR', '審核中'), 
+        ('M', '已入社'),
+        ('NP', '未付款'),
+    ]
+
     name = models.CharField(max_length=50)
     nid = models.CharField(max_length=15, unique=True)
     dept = models.CharField(max_length=20)
     level = models.CharField(max_length=2, choices=LEVEL_CHOICES)
-    phone = models.CharField(max_length=15)
+    phone = models.CharField(max_length=15, unique=True)
     email = models.EmailField(unique=True)
     status = models.CharField(max_length=2, choices=STATUS_CHOICES, default="UR")
 
