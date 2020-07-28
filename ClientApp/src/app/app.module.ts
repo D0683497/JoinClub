377d1c3f8656ae15181f5bdb902dfa7fdf0f878c
D0683497 <ff129043923@gmail.com>
2020-07-28 11:01:35 +0800

    新增 angular/flex-layout 及建立 shared material module

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { SharedMaterialModule } from './shared/shared-material.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    SharedMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
