3e4282ac51ccba09d8aa71686c3280be3fe435ce
D0683497 <ff129043923@gmail.com>
2020-09-12 00:39:27 +0800

    新增註冊功能

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using JoinClub.Data;
 using JoinClub.Entities.Application;
 using JoinClub.Models.Account;
 using Microsoft.AspNetCore.Authorization;
@@ -9,22 +14,77 @@ using Microsoft.Extensions.Logging;
 namespace JoinClub.Controllers
 {
     [ApiController]
-    [Route("api/[controller]/[action]")]
+    [Route("api/[controller]")]
     public class AccountController : ControllerBase
     {
         private readonly ILogger<AccountController> _logger;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IMapper _mapper;
+        private readonly ApplicationDbContext _applicationDbContext;
 
-        public AccountController(ILogger<AccountController> logger, UserManager<ApplicationUser> userManager)
+        public AccountController(
+            ILogger<AccountController> logger, 
+            UserManager<ApplicationUser> userManager, 
+            IMapper mapper, 
+            ApplicationDbContext applicationDbContext)
         {
             _logger = logger;
             _userManager = userManager;
+            _mapper = mapper;
+            _applicationDbContext = applicationDbContext;
         }
 
         [AllowAnonymous]
-        [HttpPost(Name = nameof(Register))]
+        [HttpPost("register", Name = nameof(Register))]
         public async Task<IActionResult> Register(RegisterViewModel model)
         {
+            var entity = _mapper.Map<ApplicationUser>(model);
+
+            using (var scope = await _applicationDbContext.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    var createUserResult = await _userManager.CreateAsync(entity, model.Password);
+                    if (createUserResult.Succeeded)
+                    {
+                        var currentUser = await _userManager.FindByNameAsync(entity.UserName);
+                        _logger.LogInformation($"建立{currentUser.Id}成功");
+                        var addRoleResult = await _userManager.AddToRoleAsync(currentUser, "Member");
+                        if (addRoleResult.Succeeded)
+                        {
+                            _logger.LogInformation($"添加{currentUser.Id}角色成功");
+                            var claims = new List<Claim>
+                            {
+                                new Claim(ClaimTypes.NameIdentifier, string.IsNullOrEmpty(currentUser.Id) ? "" : currentUser.Id),
+                                new Claim(ClaimTypes.Name, string.IsNullOrEmpty(currentUser.UserName) ? "" : currentUser.UserName),
+                                new Claim(ClaimTypes.Email, string.IsNullOrEmpty(currentUser.Email) ? "" : currentUser.Email),
+                                new Claim(ClaimTypes.MobilePhone, string.IsNullOrEmpty(currentUser.PhoneNumber) ? "" : currentUser.PhoneNumber)
+                            };
+                            var addClaimResult = await _userManager.AddClaimsAsync(currentUser, claims);
+                            if (addClaimResult.Succeeded)
+                            {
+                                _logger.LogInformation($"添加{currentUser.Id}聲明成功");
+                                await scope.CommitAsync();
+                                _logger.LogInformation($"註冊{currentUser.Id}成功");
+                                return Ok();
+                            }
+                            _logger.LogError($"添加{currentUser.Id}聲明失敗");
+                        }
+                        _logger.LogError($"添加{currentUser.Id}角色失敗");
+                    }
+                    
+                    _logger.LogWarning($"建立{entity.UserName}失敗");
+                    await scope.RollbackAsync();
+                    return BadRequest();
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError($"註冊{entity.UserName}失敗");
+                    _logger.LogError($"{e.ToString()}");
+                    throw;
+                }
+            }
+            
             return Ok();
         }
         
@@ -34,12 +94,27 @@ namespace JoinClub.Controllers
         {
             var user = await _userManager.FindByIdAsync(model.UserId);
 
-            var result = await _userManager.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
-            if (result.Succeeded)
+            if (user == null)
             {
-                return Ok();
+                _logger.LogError($"{model.UserId}修改密碼，找不到使用者");
+                return BadRequest();
             }
 
+            var checkPasswordResult = await _userManager.CheckPasswordAsync(user, model.OldPassword);
+            if (!checkPasswordResult)
+            {
+                _logger.LogWarning($"{user.Id}修改密碼，舊密碼錯誤");
+                return BadRequest();
+            }
+            
+            var changePasswordResult = await _userManager.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+            if (changePasswordResult.Succeeded)
+            {
+                _logger.LogInformation($"{user.Id}修改密碼成功");
+                return Ok();
+            }
+            
+            _logger.LogError($"{user.Id}修改密碼失敗");
             return BadRequest();
         }
     }
---
+++ Profiles/UserProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using JoinClub.Entities.Application;
+using JoinClub.Models.Account;
+
+namespace JoinClub.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            // RegisterViewModel 傳換成 ApplicationUser
+            CreateMap<RegisterViewModel, ApplicationUser>()
+                .ForMember(dest => dest.Email,
+                    opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.UserName,
+                    opt => opt.MapFrom(src => src.UserName))
+                .ForMember(dest => dest.PhoneNumber,
+                    opt => opt.MapFrom(src => src.PhoneNumber))
+                .ForMember(dest => dest.NID,
+                    opt => opt.MapFrom(src => src.NID))
+                .ForMember(dest => dest.Name,
+                    opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.College,
+                    opt => opt.MapFrom(src => src.College))
+                .ForMember(dest => dest.Department,
+                    opt => opt.MapFrom(src => src.Department))
+                .ForMember(dest => dest.Class,
+                    opt => opt.MapFrom(src => src.Class));
+        }
+    }
+}
