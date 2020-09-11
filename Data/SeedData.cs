ee19d0b1ed90dbc29f44af03c2c4e9ecc6e67507
D0683497 <ff129043923@gmail.com>
2020-09-12 00:57:15 +0800

    新增 SeedData 功能

---
+++ Data/SeedData.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using JoinClub.Entities.Application;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace JoinClub.Data
+{
+    public class SeedData
+    {
+        public static void EnsureSeedData(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<SeedData>>();
+                
+                #region Database
+
+                logger.LogInformation("開始創建資料庫");
+                var dbContext = services.GetRequiredService<ApplicationDbContext>();
+                dbContext.Database.EnsureCreated();
+                logger.LogInformation("創資料庫完成");
+
+                #endregion
+                
+                #region Role
+
+                logger.LogInformation("開始創建角色及角色聲明");
+                CreateRole(services, logger);
+                logger.LogInformation("創建角色及角色聲明完成");
+
+                #endregion
+                
+                #region User
+
+                logger.LogInformation("開始創建使用者");
+                CreateUser(services, logger);
+                logger.LogInformation("創建使用者完成");
+
+                #endregion
+            }
+        }
+        
+        private static void CreateRole(IServiceProvider services, ILogger<SeedData> logger)
+        {
+            try
+            {
+                var roleManager = services.GetRequiredService<RoleManager<ApplicationRole>>();
+
+                #region Admin
+
+                var result = roleManager.CreateAsync(new ApplicationRole { Name = "Admin" }).Result;
+                if (result.Succeeded)
+                {
+                    logger.LogInformation("建立Admin角色成功");
+
+                    var admin = roleManager.FindByNameAsync("Admin").Result;
+                    var adminClaim = new Claim(ClaimTypes.Role, "Admin");
+
+                    result = roleManager.AddClaimAsync(admin, adminClaim).Result;
+                    if (result.Succeeded)
+                    {
+                        logger.LogInformation("建立Admin角色聲明成功");
+                    }
+                    else
+                    {
+                        logger.LogError("建立Admin角色聲明失敗");
+                    }
+                }
+                else
+                {
+                    logger.LogError("建立Admin角色失敗");
+                }
+
+                #endregion
+
+                #region Member
+
+                result = roleManager.CreateAsync(new ApplicationRole { Name = "Member" }).Result;
+                if (result.Succeeded)
+                {
+                    logger.LogInformation("建立Member角色成功");
+
+                    var host = roleManager.FindByNameAsync("Member").Result;
+                    var hostClaim = new Claim(ClaimTypes.Role, "Member");
+
+                    result = roleManager.AddClaimAsync(host, hostClaim).Result;
+                    if (result.Succeeded)
+                    {
+                        logger.LogInformation("建立Member角色聲明成功");
+                    }
+                    else
+                    {
+                        logger.LogError("建立Member角色聲明失敗");
+                    }
+                }
+                else
+                {
+                    logger.LogError("建立Member角色失敗");
+                }
+
+                #endregion
+            }
+            catch (Exception e)
+            {
+                logger.LogError($"發生未知錯誤\n{e.ToString()}");
+                throw;
+            }
+        }
+
+        private static void CreateUser(IServiceProvider services, ILogger<SeedData> logger)
+        {
+            try
+            {
+                var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                var configuration = services.GetRequiredService<IConfiguration>();
+                
+                #region Admin
+                
+                var admin = new ApplicationUser
+                {
+                    Email = configuration["UserSettings:Admin:Email"],
+                    EmailConfirmed = true,
+                    UserName = configuration["UserSettings:Admin:UserName"]
+                };
+
+                var result = userManager.CreateAsync(admin, configuration["UserSettings:Admin:Password"]).Result;
+                if (result.Succeeded)
+                {
+                    logger.LogInformation("建立Admin使用者成功");
+                }
+                else
+                {
+                    logger.LogError("建立Admin使用者失敗");
+                }
+
+                var currentUser = userManager.FindByNameAsync(configuration["UserSettings:Admin:UserName"]).Result;
+                result = userManager.AddToRoleAsync(currentUser, "Admin").Result;
+                if (result.Succeeded)
+                {
+                    logger.LogInformation("Admin使用者添加角色成功");
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, string.IsNullOrEmpty(currentUser.Id) ? "" : currentUser.Id),
+                        new Claim(ClaimTypes.Name, string.IsNullOrEmpty(currentUser.UserName) ? "" : currentUser.UserName),
+                        new Claim(ClaimTypes.Email, string.IsNullOrEmpty(currentUser.Email) ? "" : currentUser.Email),
+                        new Claim(ClaimTypes.MobilePhone, string.IsNullOrEmpty(currentUser.PhoneNumber) ? "" : currentUser.PhoneNumber)
+                    };
+                    var addClaimResult = userManager.AddClaimsAsync(currentUser, claims).Result;
+                    if (addClaimResult.Succeeded)
+                    {
+                        logger.LogInformation("Admin使用者添加聲明成功");
+                    }
+                    else
+                    {
+                        logger.LogError("Admin使用者添加聲明失敗");
+                    }
+                }
+                else
+                {
+                    logger.LogError("Admin使用者添加角色失敗");
+                }
+
+                #endregion
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using JoinClub.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace JoinClub
 {
@@ -13,7 +8,11 @@ namespace JoinClub
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            SeedData.EnsureSeedData(host);
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
